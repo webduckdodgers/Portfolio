037bc81753bdcb36ab6e0f17334f7e321aa11544
Duffy <web.duckdodgers@gmail.com>
2023-11-23 11:42:24 +0100

    test fonction mail

--- envoi_formulaire.php
+++ envoi_formulaire.php
@@ -1,34 +1,8 @@
 
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nom = $_POST['nom'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+    $destinataire = "marc.romero.ostos@gmail.com";
+    $sujet = "Essai";
+    $contenu = "Coucou! Ça marche";
 
-    $destinataire = 'marc.romero.ostos@gmail.com'; // Remplacez par votre adresse e-mail
-
-    $sujet = 'Nouveau message depuis le formulaire de contact';
-    $contenu = "Nom: $nom\n";
-    $contenu .= "Email: $email\n";
-    $contenu .= "Message: $message\n";
-
-    $headers = 'From: ' . $email . "\r\n" .
-        'Reply-To: ' . $email . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();
-
-    mail($destinataire, $sujet, $contenu, $headers);
-}
+    mail($destinataire, $sujet, $contenu, "");
 ?>
-
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Votre Document à été envoyer :)</h1>
-</body>
-</html>
-
